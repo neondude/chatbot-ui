e7bd61afcfcc3673b371fcd66345f9b8b613cea9
neondude <siddharth.nagrajann@gmail.com>
2023-10-11 14:00:04 +0530

    update default constants

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,12 +1,31 @@
+// Create a new Date object
+const currentDate = new Date();
+
+// Get the current month, day, and year
+// Array of month names
+const monthNames = [
+  'January', 'February', 'March', 'April', 'May', 'June', 'July',
+  'August', 'September', 'October', 'November', 'December'
+];
+
+// Get the current month, day, and year
+const monthIndex = currentDate.getMonth();
+const month = monthNames[monthIndex];
+const day = String(currentDate.getDate()).padStart(2, '0');
+
+const year = currentDate.getFullYear();
+
+const formattedDate = `${day}-${month}-${year}`;
+
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  `You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture. Knowledge cutoff: 2021-09.Current date: ${formattedDate}.`;
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
 export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.4");
 
 export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
